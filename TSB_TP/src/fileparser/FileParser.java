a8c5f4e3a448e403299d59a973a9e40a656b832b
Gonzalo <saad.gonzalo.ale@gmail.com>
2017-10-21 15:47:48 -0300

    Agregando comentarios

--- TSB_TP/src/fileparser/FileParser.java
+++ TSB_TP/src/fileparser/FileParser.java
@@ -18,7 +18,6 @@ import java.text.Normalizer;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 /**
  *
@@ -33,7 +32,7 @@ public class FileParser implements Iterable<String>, Closeable {
     Scanner sc;
 
     /**
-     *
+     * Contructor con path tipo string. 
      * @param path - path del archivo a leer.
      * @throws FileNotFoundException - si no encuentra el archivo.
      * @throws NullPointerException - si path es null.
@@ -44,33 +43,62 @@ public class FileParser implements Iterable<String>, Closeable {
         // ([a-z]+[0-9]+|[0-9]+[a-z]+)[a-z0-9]*\b|[^A-Za-z]+
         //sc.skip("[^A-Za-z]+");
     }
-
+    
+    /**
+     * Constructor con archivo tipo File. 
+     * @param file - el archivo. 
+     * @throws FileNotFoundException - si no encuentra el archivo. 
+     */
     public FileParser(File file) throws FileNotFoundException {
         sc = new Scanner(file, "ISO-8859-1");
 
     }
-
+    
+    /**
+     * "Iterador" del archivo.
+     * @return - una instancia del iterador. 
+     */
     @Override
     public Iterator<String> iterator() {
         return new FileIterator();
     }
-
+    
+    /**
+     * Cierra el scanner para liberar el archivo. 
+     * @throws IOException 
+     */
     @Override
     public void close() throws IOException {
         sc.close();
     }
-
+        
+    /**
+     * Elimina los acentos de un string. 
+     * á -> a
+     * @param s - string a limpiar. 
+     * @return - el string sin acentos. 
+     */
     private String stripAccents(String s) {
         s = Normalizer.normalize(s, Normalizer.Form.NFD);
         s = s.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
         return s;
     }
     
-    private String stripNonAlphabetic(String s){
-        return s.replaceAll("([a-z]+[0-9]+|[0-9]+[a-z]+)[a-z0-9]*\b|[^A-Za-z]+","" );
+    /**
+     * Elimina todo aquello que no sea palabras de 
+     * solo letras. 
+     * @param s - string a limpiar.
+     * @return - string solamente de letras. 
+     */
+    private String stripNonAlphabetic(String s) {
+        return s.replaceAll("([a-z]+[0-9]+|[0-9]+[a-z]+)[a-z0-9]*\b|[^A-Za-z]+", "");
     }
-    
-    private String cleanString(String s){
+    /**
+     * Elimina acentos, palabras alfanumericas y puntuacion. 
+     * @param s - string a limpiar. 
+     * @return - string limpio.
+     */
+    private String cleanString(String s) {
         return stripNonAlphabetic(stripAccents(s));
     }
 
@@ -86,8 +114,8 @@ public class FileParser implements Iterable<String>, Closeable {
         public String next() throws NoSuchElementException {
             if (!hasNext()) {
                 throw new NoSuchElementException("No quedan mas palabras.");
-            }          
-            
+            }
+
             return FileParser.this.cleanString(sc.next()).toLowerCase();
 
         }
--- TSB_TP/src/tplogic/TPLogic.java
+++ TSB_TP/src/tplogic/TPLogic.java
@@ -16,45 +16,122 @@ import java.util.ArrayList;
  * @author a4
  */
 public class TPLogic {
+
     private ArrayList<File> filesUsed;
     private TSBHashtable<String, Integer> hashtable;
 
     public TPLogic() {
+        /**
+         * HACER:
+         * En realidad aca tiene que deserializar el arhivo, si es que
+         * lo encuentra, y sino instanciar como esta hecho.
+         */
+
         hashtable = new TSBHashtable();
         filesUsed = new ArrayList();
     }
 
-    public int readFile(String path) throws FileNotFoundException {        
+    /**
+     * Lee un archivo cuyo path sea pasado como string, y
+     * agrega las palabras a la tabla hash.
+     *
+     * @param path - la ruta del archivo.
+     * @return - la cantidad de palabras nuevas.
+     * @throws FileNotFoundException - si el archivo no existe.
+     */
+    public int readFile(String path) throws FileNotFoundException {
         return readFile(new File(path));
     }
 
+    /**
+     * Lee un archivo pasado por parametro y
+     * agrega las palabras a la tabla hash
+     *
+     * @param file - el archivo a leer.
+     * @return - la cantidad de palabras agregadas.
+     * @throws FileNotFoundException - si el archivo no existe.
+     */
     public int readFile(File file) throws FileNotFoundException {
+
+        // Se usa un objeto de FileParser. 
         FileParser fp = new FileParser(file);
+
+        // Variable para guardar la frecuencia 
+        // de la alabra a agregar en caso que exista. 
         int value;
+
+        // Guarda el tamaño hashtable 
+        // previo a la insersion.
         int countPrev = hashtable.size();
-        
+
+        // Se itera por cada palabra que se encuentre. 
         for (String pal : fp) {
+
+            /**
+             * Si la palabra es vacia se la ignora.
+             * Esto es asi porque antes de retornar el iterador
+             * del archivo hace una limpieza (No me salio de otra
+             * manera, Gonza) y puede ser que luego de la limpieza
+             * no quede nada. Por ejemplo si el proximo item es "!!!",
+             * se elimina todo.
+             */
             if (!pal.equals("")) {
+
+                /**
+                 * Se hace un getOrDefault, si la encuentra, devuelve
+                 * la frecuencia, y sino devuelve 0.
+                 */
                 value = hashtable.getOrDefault(pal, 0);
+
+                /**
+                 * Se hace un put, si la palabra existe, se le
+                 * sumara uno a la freucuencia, sino se agrega una entrada
+                 * a la tabla con value=1.
+                 */
                 hashtable.put(pal, value + 1);
             }
         }
+
+        // Se agrega el archivo a la lista de archivos analizados.
         filesUsed.add(file);
-        return hashtable.size()- countPrev;
+
+        // Se retorna la diferencia de size del hashtable. 
+        return hashtable.size() - countPrev;
 
     }
 
+    /**
+     * Devuelve la frecuencia de una palabra.
+     *
+     * @param word - la palabra a consultar.
+     * @return - la frecuencia de la palabra.
+     */
     public int getWordFrecuency(String word) {
+        /**
+         * El metodo get or default devuelve la frecuencia de la
+         * palabra si la encuentra, sino retorna un valor default
+         * que aca se seteo en 0, que vendira a ser cero apariciones.
+         */
         return hashtable.getOrDefault(word, 0);
     }
 
+    /**
+     * Retorna la cantidad de elementos que hay.
+     *
+     * @return - elementos del hashtable.
+     */
     public int checkWordsCount() {
         return hashtable.size();
     }
 
+    /**
+     * Devuelve el array de archivos.
+     * Se usa para llenar el ListView de la GUI.
+     *
+     * @return - el array.
+     */
     public ArrayList<File> getFilesUsed() {
         return filesUsed;
     }
-    
-    
+
 }
