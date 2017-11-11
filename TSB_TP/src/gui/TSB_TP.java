e0d9476ce0d0964609b778f9ae3a21698a952927
Gonzalo <saad.gonzalo.ale@gmail.com>
2017-11-11 19:43:45 -0300

    Solucionando una falla que mande yo jajaja

--- TSB_TP/src/gui/TSB_TP.java
+++ TSB_TP/src/gui/TSB_TP.java
@@ -21,7 +21,7 @@ public class TSB_TP extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("AnalizadorPalabrasGUI.fxml"));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
